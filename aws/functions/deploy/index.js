66e7ac0044e81a1b3d41d019d9a9b4992e92aaba
Guillem Mateos <bbguillem@gmail.com>
2018-12-03 12:49:36 +0100

    First version of AWS bootstrap

---
+++ aws/functions/deploy/index.js
@@ -0,0 +1,47 @@
+const aws = require('aws-sdk');
+
+const ecs = new aws.ECS({ apiVersion: '2014-11-13' });
+
+function runECS(msg) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      taskDefinition: process.env.TASK_DEF,
+      launchType: 'EC2',
+      count: 1,
+      cluster: process.env.CLUSTER,
+      overrides: {
+        containerOverrides: [{
+          name: 'ECSTest',
+          environment: [{
+            name: 'CODE_REPO_URL',
+            value: msg.url,
+          },
+          {
+            name: 'CODE_REPO_HASH',
+            value: msg.hash,
+          },
+          {
+            name: 'CODE_REPO_STAGE',
+            value: msg.stage,
+          }],
+        }],
+      },
+    };
+
+    ecs.runTask(params, (err, data) => {
+      if (err) {
+        reject('Error while starting task:'.concat(err));
+      } else {
+        resolve('Task ECSTask started: '.concat(JSON.stringify(data.tasks)));
+      }
+    });
+  });
+}
+
+exports.handler = async (event) => {
+  console.log(event.Records[0].Sns.Message.url);
+  console.log(event.Records[0].Sns.Message.hash);
+  console.log(event.Records[0].Sns.Message.stage);
+
+  return runECS(event.Records[0].Sns.Message);
+};
