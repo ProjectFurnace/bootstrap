962f5f35f11a2f8a3e6e75670dbc4fd01a7b604e
Guillem Mateos <bbguillem@gmail.com>
2018-12-06 14:00:35 +0100

    Added additional fields

--- aws/functions/deploy-exec/index.js
+++ aws/functions/deploy-exec/index.js
@@ -2,32 +2,66 @@ const aws = require('aws-sdk');
 
 const ecs = new aws.ECS({ apiVersion: '2014-11-13' });
 
-function runECS(msg) {
+function runECS(event) {
+  const msg = JSON.parse(event);
+
+  if (process.env.DEBUG) {
+    /* eslint-disable no-console */
+    console.log(msg);
+  }
+
   return new Promise((resolve, reject) => {
     const params = {
-      taskDefinition: process.env.TASK_DEF,
+      taskDefinition: process.env.TASK_DEFINITION,
       launchType: 'EC2',
       count: 1,
       cluster: process.env.CLUSTER,
       overrides: {
         containerOverrides: [{
-          name: 'ECSTest',
+          name: process.env.CONTAINER_NAME,
           environment: [{
-            name: 'CODE_REPO_URL',
-            value: msg.url,
+            name: 'GIT_REMOTE',
+            value: msg.remoteUrl,
           },
           {
-            name: 'CODE_REPO_HASH',
-            value: msg.hash,
+            name: 'GIT_TAG',
+            value: msg.commitRef,
           },
           {
-            name: 'CODE_REPO_STAGE',
-            value: msg.stage,
+            name: 'STACK_ENV',
+            value: msg.environment,
+          },
+          {
+            name: 'GIT_USERNAME',
+            value: process.env.GIT_USERNAME,
+          },
+          {
+            name: 'GIT_TOKEN',
+            value: process.env.GIT_TOKEN,
+          },
+          {
+            name: 'AWS_ACCESS_KEY_ID',
+            value: process.env.AWS_KEY,
+          },
+          {
+            name: 'AWS_SECRET_ACCESS_KEY',
+            value: process.env.AWS_SECRET,
+          },
+          {
+            name: 'NPM_TOKEN',
+            value: process.env.NPM_TOKEN,
+          },
+          {
+            name: 'DEPLOYMENT_ID',
+            value: msg.deploymentId.toString(),
+          },
+          {
+            name: 'BUILD_BUCKET',
+            value: process.env.BUILD_BUCKET,
           }],
         }],
       },
     };
-
     ecs.runTask(params, (err, data) => {
       if (err) {
         reject('Error while starting task:'.concat(err));
@@ -41,11 +75,7 @@ function runECS(msg) {
 exports.handler = async (event) => {
   if (process.env.DEBUG) {
     /* eslint-disable no-console */
-    console.log(event.Records[0].Sns.Message.url);
-    console.log(event.Records[0].Sns.Message.hash);
-    console.log(event.Records[0].Sns.Message.stage);
-    /* eslint-enable no-console */
+    console.log(event.Records[0].Sns);
   }
-
   return runECS(event.Records[0].Sns.Message);
 };
--- aws/functions/deploy-trigger/index.js
+++ aws/functions/deploy-trigger/index.js
@@ -1,38 +1,151 @@
 const AWS = require('aws-sdk');
+const https = require('https');
 
 const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
+
+function getEnvs(yaml) {
+  const pos = yaml.indexOf('environments:');
+  if (pos > 0) {
+    return yaml.substr(pos + 14).trim().replace(/-| /g, '').split(/\r?\n/);
+  }
+  return false;
+}
+
+function doRequest(options, postData) {
+  return new Promise((resolve, reject) => {
+    const buffers = [];
+    const req = https.request(options, (res) => {
+      res.on('data', (buffer) => {
+        buffers.push(buffer);
+      });
+
+      res.on('end', () => {
+        if (res.statusCode === 200 || res.statusCode === 201) {
+          resolve(Buffer.concat(buffers).toString());
+        } else {
+          reject(Buffer.concat(buffers));
+        }
+      });
+    }).on('error', (error) => {
+      reject(error);
+    });
+
+    if (postData) {
+      req.write(postData);
+    }
+    req.end();
+  });
+}
 
 exports.handler = async (event, context, callback) => {
   if (event.body != null) {
     const body = JSON.parse(event.body);
 
-    if (body.remoteUrl && body.commitRef) {
+    if (body.deployment) {
       const params = {
-        Message: {
-          remoteUrl: body.remoteUrl,
-          commitRef: body.commitRef,
-        },
+        Message: JSON.stringify({
+          remoteUrl: body.repository.clone_url,
+          commitRef: body.deployment.ref,
+          deploymentId: body.deployment.id,
+          environment: body.deployment.environment,
+        }),
         TopicArn: process.env.TOPIC,
-        MessageStructure: 'JSON',
       };
 
       // Create promise and SNS service object
       const publishTextPromise = sns.publish(params).promise();
 
       // Handle promise's fulfilled/rejected states
-      publishTextPromise.then((data) => {
+      await publishTextPromise.then((data) => {
         if (process.env.DEBUG) {
           // eslint-disable-next-line no-console
-          console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn} with ID ${data.MessageId}`);
+          console.log(`Message ${params.Message} sent to the topic ${params.TopicArn} with ID ${data.MessageId}`);
         }
-        callback(null, { statusCode: 200, body: 'Successful' });
+        callback(null, { statusCode: 200, body: JSON.stringify({ msg: 'Deployment successfully started' }) });
       }).catch((err) => {
         if (process.env.DEBUG) {
           // eslint-disable-next-line no-console
           console.error(err, err.stack);
         }
-        callback(err);
+        callback(null, { statusCode: 500, body: JSON.stringify({ error: 'Something went wrong...' }) });
       });
+    } else if (body.hook && body.hook.Type === 'Repository') {
+      callback(null, { statusCode: 200, body: JSON.stringify({ msg: 'Test hook received' }) });
+    } else if (body.repository || (body.remoteUrl && body.commitRef && body.environment)) {
+      const stackYamlOptions = {
+        host: 'raw.githubusercontent.com',
+        method: 'GET',
+        headers: {
+          // eslint-disable-next-line prefer-template
+          Authorization: 'token ' + process.env.GIT_TOKEN,
+          'User-Agent': 'Project Furnace',
+        },
+      };
+
+      let owner = '';
+      let repo = '';
+
+      if (body.repository) {
+        // eslint-disable-next-line prefer-template
+        stackYamlOptions.path = '/' + body.repository.full_name + '/' + body.repository.default_branch + '/stack.yaml';
+      } else {
+        const parts = body.remoteUrl.split('/');
+
+        owner = parts[3];
+        repo = parts[4];
+        // eslint-disable-next-line prefer-template
+        stackYamlOptions.path = '/' + owner + '/' + repo + '/' + body.commitRef + '/stack.yaml';
+      }
+
+      const stackYaml = await doRequest(stackYamlOptions);
+
+      const environments = getEnvs(stackYaml);
+
+      if (environments) {
+        const deploymentData = {
+          ref: 'master',
+        };
+
+        if (body.repository) {
+          // eslint-disable-next-line prefer-template
+          deploymentData.owner = body.repository.owner.name;
+          deploymentData.repo = body.repository.name;
+          deploymentData.environment = environments[0];
+        } else {
+          // eslint-disable-next-line prefer-template
+          deploymentData.owner = owner;
+          deploymentData.repo = repo;
+          deploymentData.environment = body.environment;
+        }
+
+        const postData = JSON.stringify(deploymentData);
+
+        const deploymentOptions = {
+          host: 'api.github.com',
+          method: 'POST',
+          headers: {
+            // eslint-disable-next-line prefer-template
+            Authorization: 'Bearer ' + process.env.GIT_TOKEN,
+            Accept: 'application/vnd.github.v3+json',
+            'Content-type': 'application/json',
+            'Content-Length': postData.length,
+            'User-Agent': 'Project Furnace',
+          },
+        };
+
+        if (body.repository) {
+          // eslint-disable-next-line prefer-template
+          deploymentOptions.path = '/repos/' + body.repository.full_name + '/deployments';
+        } else {
+          // eslint-disable-next-line prefer-template
+          deploymentOptions.path = '/repos/' + owner + '/' + repo + '/deployments';
+        }
+
+        await doRequest(deploymentOptions, postData);
+
+        callback(null, { statusCode: 200, body: JSON.stringify({ msg: 'Deployment successfully triggered' }) });
+      }
     }
+    callback(null, { statusCode: 422, body: JSON.stringify({ error: 'Request is missing some parameter' }) });
   }
 };
