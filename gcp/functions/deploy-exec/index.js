b2a1c2fda35c6c5ccae5c11f3a8e1dcf540b6676
Guillem Mateos <bbguillem@gmail.com>
2019-05-01 11:31:30 +0200

    GCP support

--- gcp/functions/deploy-exec/index.js
+++ gcp/functions/deploy-exec/index.js
@@ -1,102 +1,178 @@
-const util = require("util")
-    ;
+const targz = require('targz');
+const fsutils = require("@project-furnace/fsutils");
+const {Storage} = require('@google-cloud/storage');
+const kms = require('@google-cloud/kms');
+const {auth} = require('google-auth-library');
+const {google} = require("googleapis");
+const gitUtils = require("@project-furnace/gitutils");
+const path = require("path");
+const randomstring = require("randomstring");
 
-exports.handler = async (context, eventInput) => {
-  context.log("got event", eventInput);
+const storage = new Storage();
 
-  if (!eventInput.remoteUrl || !eventInput.commitRef) {
-    context.done(new Error("remoteUrl and commitRef must be sent in the event input"));
-    return;
+exports.handler = async (inputEvent, callback) => {
+  if (process.env.DEBUG) {
+    // eslint-disable-next-line no-console
+    console.log(inputEvent);
   }
 
-  const credentials = await MsRest.loginWithAppServiceMSI();
-  context.log("got credentials", util.inspect(credentials, { depth: null }));
+  const event = inputEvent.data ? JSON.parse( Buffer.from(inputEvent.data, 'base64').toString() ) : {};
 
-  const tokenResult = await axios({
-    url: `${credentials.msiEndpoint}?resource=${credentials.resource}&api-version=${credentials.msiApiVersion}`,
-    headers: { "Secret": credentials.msiSecret }
+  if (process.env.DEBUG) {
+    // eslint-disable-next-line no-console
+    console.log(event);
+  }
+
+  const authClient = await getAuthClient();
+
+  google.options({ auth: authClient });
+
+  const rand = randomstring.generate({ length: 6, capitalization: 'lowercase' });
+  const objectName = `${process.env.FURNACE_INSTANCE}-${rand}.tar.gz`
+
+  await upload(event, objectName);
+
+  // const builds = await google.cloudbuild("v1").projects.builds.list({ projectId: "furnace-scratch" });
+
+  // const projectId = await auth.getProjectId();
+
+  //   steps:
+  // - name: gcr.io/$PROJECT_ID/pulumi-node
+  //   entrypoint: /bin/sh
+  //   args:
+  //   - '-c'
+  //   - 'yarn install && pulumi login && pulumi stack select <stack_name> && pulumi preview'
+  //   env: ["PULUMI_ACCESS_TOKEN=$_INSECURE_SUBSTITUTION_PULUMI_ACCESS_TOKEN_FOR_TESTING"]
+
+  const buildResult = await google.cloudbuild("v1").projects.builds.create({
+    projectId: process.env.PROJECT_ID,
+    requestBody: {
+      source: {
+        storageSource: {
+          bucket: process.env.BUILD_BUCKET,
+          object: objectName
+        }
+      },
+      steps: [
+        {
+          name: "dannywaite/deploy-gcp:latest",
+          entrypoint: "/bin/bash",
+          args: [
+            "-c",
+            "/app/deploy_gcp_local.sh"
+          ],
+          env: [
+            "FURNACE_LOCAL=1",
+            "REPO_DIR=/workspace",
+            "TEMPLATE_REPO_DIR=/app/test/fixtures/templates",
+            "STACK_ENV=test",
+            "STACK_REGION=".concat(process.env.LOCATION),
+            "DEPLOYMENT_ID=".concat(event.deploymentId.toString()),
+            "PLATFORM=".concat(process.env.PLATFORM),
+            "BUILD_BUCKET=".concat(process.env.BUILD_BUCKET),
+            "FURNACE_INSTANCE=".concat(process.env.FURNACE_INSTANCE),
+            "GCP_PROJECT=".concat(process.env.PROJECT_ID)
+          ]
+        }
+      ]
+    }
   });
 
-  if (!tokenResult.data.access_token) {
-    context.done(new Error("unable to get access token"));
-    return;
-  }
+  console.log(buildResult);
+};
 
-  const accessToken = tokenResult.data.access_token
-      , subscriptionId = "836eaf85-82b8-4c2d-a4d1-2082104c7362"
-      , resourceGroupName = "feasterrg"
-      , containerGroupName = "dannytest1"
-      , containerUrl = `/resourceGroups/${resourceGroupName}/providers/Microsoft.ContainerInstance/containerGroups/${containerGroupName}?api-version=2018-10-01`
-      , identityName = "FurnaceDeployUserIdentity"
-      , userIdArn = `/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/${identityName}`
-      ;
+async function getAuthClient() {
+  const client = await auth.getClient({
+    scopes: 'https://www.googleapis.com/auth/cloud-platform'
+  });
+  return client;
+}
 
-  const containerGroupDep = {
-    type: "Microsoft.ContainerInstance/containerGroups",
-    location: "westus",
-    identity: {
-      type: "UserAssigned",
-      userAssignedIdentities: {
-        [userIdArn]: {}
-      }
-    },
-    properties: {
-      containers: [
-        {
-          name: "furnace-deploy-container",
-          properties: {
-            environmentVariables: [
-              { name: "GIT_REMOTE", value: eventInput.remoteUrl },
-              { name: "GIT_TAG", value: eventInput.commitRef },
-              { name: "GIT_USERNAME", value: "unset" },
-              { name: "STACK_ENV", value: eventInput.environment },
-              { name: "STACK_REGION", value: "WestUS" },
-              { name: "ARM_USE_MSI", value: "true" },
-              { name: "DEPLOYMENT_ID", value: eventInput.deploymentId.toString() },
-              { name: "PLATFORM", value: process.env.PLATFORM },
-              { name: "BUILD_BUCKET", value: process.env.BUILD_BUCKET },
-              { name: "FURNACE_INSTANCE", value: process.env.FURNACE_INSTANCE }
-            ],
-            image: eventInput.image || "dannywaite/deploy:latest",
-            resources: {
-              requests: {
-                memoryInGB: "1.5",
-                cpu: 1
-              }
-            }
-          }
+function compress(repoDir, destination) {
+  return new Promise((resolve, reject) => {
+    targz.compress({
+      src: repoDir,
+      dest: destination
+    }, function(err){
+        if(err) {
+            reject(err);
+        } else {
+            resolve(destination);
         }
-      ],
-      imageRegistryCredentials: [],
-      osType: "Linux",
-      volumes: [],
-      restartPolicy: "Never"
-    }
-  }
-  
+    });  
+  })
+}
+
+// Read file
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const fileReader = file.createReadStream();
+    const buffers = [];
+    fileReader.on('data', function(data) {
+      buffers.push(data);
+    }).on('end', function() {
+      resolve(Buffer.concat(buffers));
+    }).on('error', (error) => {
+      reject(error);
+    });
+  });
+}
+
+// Retrieve a secret from Key Vault
+async function getSecret(name) {
+  const kmsClient = new kms.KeyManagementServiceClient();
+  const secretBucket = storage.bucket(process.env.SECRETS_BUCKET_NAME);
+
+  const file = secretBucket.file(name);
+
   try {
-    
-    const httpClient = getHttpClient(`https://management.azure.com/subscriptions/${subscriptionId}`, accessToken);
-    const result = await httpClient.put(containerUrl, containerGroupDep);
-    
-    context.log(util.inspect(result.data, { depth: null }));
-    context.done();
-  } catch (err) {
-    if (err.response) {
-      context.log(util.inspect(err.response, { depth: null }));
-    } else {
-      context.log(util.inspect(err, { depth: null }));
-    } 
-    context.done(new Error("unable to start deployment container"));
+    if (process.env.DEBUG)
+      console.log(`Got secret file from bucket ${name}`);
+
+    const cryptoname = kmsClient.cryptoKeyPath(
+      process.env.PROJECT_ID,
+      process.env.LOCATION,
+      process.env.KEYRING_ID,
+      process.env.CRYPTOKEY_ID
+    );
+
+    const fileContents = await readFile(file);
+
+    // Decrypts the file using the specified crypto key
+    //const b64secret = Buffer.from(fileContents).toString('base64');
+    const [result] = await kmsClient.decrypt({name: cryptoname, ciphertext: Buffer.from(fileContents)});
+    return Buffer.from(result.plaintext, 'base64').toString('utf8').trim();
+  } catch(err) {
+    if (process.env.DEBUG)
+      console.log('Error retrieving secret', err);
+    return false;
   }
 }
 
-function getHttpClient(baseURL, accessToken) {
-  return axios.create({
-    baseURL,
-    headers: {
-      "authorization": "Bearer " + accessToken,
-      "content-type": "application/json"
-    }
+async function upload(data, objectName) {
+  const baseDir = fsutils.createTempDirectory();
+  const repoDir = path.join(baseDir, process.env.FURNACE_INSTANCE);
+  if (!fsutils.exists(repoDir)) fsutils.mkdir(repoDir);
+
+  const gitToken = await getSecret('GitToken');
+
+  await gitUtils.clone(repoDir, data.remoteUrl, process.env.GIT_USERNAME, gitToken);
+  await gitUtils.checkout(repoDir, data.commitRef);
+
+  const filename = await compress(repoDir, path.join(baseDir, objectName));
+
+  // const storage = new Storage({
+  //   keyFilename: "/Users/danny/.gcloud/keyfile.json",
+  //   projectId: "furnace-scratch"
+  // });
+  /*const storage = new Storage();
+
+  const filename = '/Users/danny/Downloads/gcp.tar.gz';*/
+
+  return storage.bucket(process.env.BUILD_BUCKET).upload(filename, {
+    gzip: true,
+    metadata: {
+      cacheControl: 'public, max-age=31536000',
+    },
   });
 }
