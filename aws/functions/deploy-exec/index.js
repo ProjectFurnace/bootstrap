33ec1f4b24e6264d864bc35ba501393382875cf3
Guillem Mateos <bbguillem@gmail.com>
2019-10-31 16:59:20 +0100

    First working version with cloudbuild on AWS

--- aws/functions/deploy-exec/index.js
+++ aws/functions/deploy-exec/index.js
@@ -1,8 +1,8 @@
 const aws = require('aws-sdk');
 
-const ecs = new aws.ECS({ apiVersion: '2014-11-13' });
+const codebuild = new aws.CodeBuild({ apiVersion: '2016-10-06' });
 
-function runECS(event) {
+function runBuild(event) {
   const msg = JSON.parse(event);
 
   if (process.env.DEBUG) {
@@ -12,61 +12,42 @@ function runECS(event) {
 
   return new Promise((resolve, reject) => {
     const params = {
-      taskDefinition: process.env.TASK_DEFINITION,
-      launchType: 'EC2',
-      count: 1,
-      cluster: process.env.CLUSTER,
-      overrides: {
-        containerOverrides: [{
-          name: process.env.CONTAINER_NAME,
-          environment: [{
-            name: 'GIT_REMOTE',
-            value: msg.remoteUrl,
-          },
-          {
-            name: 'GIT_TAG',
-            value: msg.commitRef,
-          },
-          {
-            name: 'STACK_ENV',
-            value: msg.environment,
-          },
-          {
-            name: 'GIT_USERNAME',
-            value: process.env.GIT_USERNAME,
-          },
-          {
-            name: 'PLATFORM',
-            value: process.env.PLATFORM,
-          },
-          {
-            name: 'DEPLOYMENT_ID',
-            value: msg.deploymentId.toString(),
-          },
-          {
-            name: 'FURNACE_INSTANCE',
-            value: process.env.FURNACE_INSTANCE,
-          },
-          {
-            name: 'BUILD_BUCKET',
-            value: process.env.BUILD_BUCKET,
-          },
-          {
-            name: 'FN_TEMPLATES_TAG',
-            value: process.env.FN_TEMPLATES_TAG,
-          },
-          {
-            name: 'SOPS_KMS_ARN',
-            value: process.env.SOPS_KMS_ARN,
-          }],
-        }],
-      },
+      projectName: process.env.FURNACE_INSTANCE + '-updateStack',
+      environmentVariablesOverride: [
+        {
+          name: 'GIT_REMOTE',
+          value: msg.remoteUrl,
+        },
+        {
+          name: 'GIT_TAG',
+          value: msg.commitRef,
+        },
+        {
+          name: 'STACK_ENV',
+          value: msg.environment,
+        },
+        {
+          name: 'GIT_USERNAME',
+          value: process.env.GIT_USERNAME,
+        },
+        {
+          name: 'DEPLOYMENT_ID',
+          value: msg.deploymentId.toString(),
+        },
+      ],
     };
-    ecs.runTask(params, (err, data) => {
+    // codebuild is not happy if we push empty env vars
+    if (process.env.FN_TEMPLATES_TAG && process.env.FN_TEMPLATES_TAG !== '') {
+      params.environmentVariablesOverride.push({
+        name: 'FN_TEMPLATES_TAG',
+        value: process.env.FN_TEMPLATES_TAG,
+      });
+    }
+    codebuild.startBuild(params, (err, data) => {
       if (err) {
-        reject('Error while starting task:'.concat(err));
+        reject('Error while starting build:'.concat(err));
       } else {
-        resolve('Task ECSTask started: '.concat(JSON.stringify(data.tasks)));
+        resolve('Build started: '.concat(JSON.stringify(data.tasks)));
       }
     });
   });
@@ -77,5 +58,5 @@ exports.handler = async (event) => {
     /* eslint-disable no-console */
     console.log(event.Records[0].Sns);
   }
-  return runECS(event.Records[0].Sns.Message);
+  return runBuild(event.Records[0].Sns.Message);
 };
